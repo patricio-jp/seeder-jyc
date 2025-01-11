cc4277dde37ac811ee883380991de4b3419c5c26
patricio-jp <patojimenezpaz@gmail.com>
2025-01-11 12:36:03 -0300

    Remove unused files. Add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        //dropSchema: true,
+        legacySpatialSupport: false,
+        logging: true,
+        entities: [__dirname + '/../entities/**/*.entity{.ts,.js}'],
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
